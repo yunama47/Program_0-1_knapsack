26436ac9bdaa0413fa6e2a1eb8a238f25222a231
yunama47 <yunama1236@gmail.com>
2022-05-21 20:59:21 +0800

    second commit

--- knapsack.py
+++ knapsack.py
@@ -1,9 +1,14 @@
-import numpy as np
-import pandas as pd
-
-def tukar_nilai(A,B):
-    A,B = B,A
-
+try:
+    import numpy as np
+    import pandas as pd
+    import PySimpleGUI as sg
+except:
+    raise ImportError(
+            " Tidak dapat mengimport modul yang diperlukan, harap menginstall modul berikut "
+            " numpy : pip install numpy "
+            " pandas : pip install pandas "
+            " PySimpleGUI : pip install PySimpleGUI "
+        )
 class Objek:
     def __init__(self,Profit,Weight):
         self.p = Profit
@@ -102,14 +107,7 @@ class Knapsack:
         for objek in solution:
             self.SET[objek].x_bruteforce = 1
 
-        # print(f'''
-        # Hasil dari algoritma brute-force:
-        # Total Profit = {SP}
-        # Total Weight = {SW}
-        # Objek yang diambil = {solution}
-        # ''')
         return (solution,SP,SW)
-    
     
     def GreedyByProfit_Knapsack(self):
         set = self.tabel_objects
@@ -129,12 +127,7 @@ class Knapsack:
         Solusi,Sisa,TW,TP = self.__cek_ulang_knapsack(Solusi,Sisa,TW,TP)
         for objek in Solusi:
             self.SET[objek].x_greedy_profit = 1
-        # print(f'''
-        # Hasil dari algoritma greedy by profit:
-        # Total Profit = {TP}
-        # Total Weight = {TW}
-        # Objek yang diambil = {Solusi}
-        # ''')
+        
         return (Solusi,TP,TW)
     
     def GreedyByWeight_Knapsack(self):
@@ -155,12 +148,7 @@ class Knapsack:
         Solusi,Sisa,TW,TP = self.__cek_ulang_knapsack(Solusi,Sisa,TW,TP)
         for objek in Solusi:
             self.SET[objek].x_greedy_weight = 1
-        # print(f'''
-        # Hasil dari algoritma greedy by weight:
-        # Total Profit = {TP}
-        # Total Weight = {TW}
-        # Objek yang diambil = {Solusi}
-        # ''')
+       
         return (Solusi,TP,TW)    
     
     def GreedyByDensity_Knapsack(self):
@@ -181,12 +169,7 @@ class Knapsack:
         Solusi,Sisa,TW,TP = self.__cek_ulang_knapsack(Solusi,Sisa,TW,TP)
         for objek in Solusi:
             self.SET[objek].x_greedy_density = 1
-        # print(f'''
-        # Hasil dari algoritma greedy by density:
-        # Total Profit = {TP}
-        # Total Weight = {TW}
-        # Objek yang diambil = {Solusi}
-        # ''')
+        
         return (Solusi,TP,TW)
 
     def __cek_ulang_knapsack(self,Solusi,Sisa,TW,TP):
@@ -208,8 +191,105 @@ class Knapsack:
                     TW = TW - self.SET[ori].w + self.SET[alt].w
                     TP = TP - self.SET[ori].p + self.SET[alt].p
         return (Solusi,Sisa,TW,TP)
-                            
 
+    
 if __name__ == '__main__':
-    N = 1000
+    nama_aplikasi = 'Aplikasi 0-1 Knapsack Problem'
+    N = 0
+    kapasitas = 0
+    err_msg = ''
+    set_objek = {}
+
+    while True:
+        layout = [
+            [sg.Text('')],
+            [sg.Text("Input jumlah barang (N) = "),sg.Input()],
+            [sg.Text("Kapasitas knapsack (K) = "),sg.Input()],
+            [sg.Text(err_msg,text_color='red')],
+            [sg.Button('next'),] 
+        ]
+        window = sg.Window(nama_aplikasi,layout)
+        event, value = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        try:
+            N = int(value[0]) 
+            kapasitas = int(value[1])
+            break
+        except:
+            err_msg = 'harap masukkan bilangan bulat'
+    window.close()
+    err_msg = ''
+    i = 1
+    while True:
+        if i > N:
+            break
+        layout = [
+            [sg.Text(f'Input data barang ke-{i} ,sudah diinput {i-1}/{N}')],
+            [sg.Text("Nama = "),sg.Input()],
+            [sg.Text("Profit ="),sg.Input()],
+            [sg.Text("Weight ="),sg.Input()],
+            [sg.Text(err_msg,text_color='red')],
+            [sg.Button('next')] 
+        ]
+        window = sg.Window(nama_aplikasi,layout)
+        event, value = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        try:
+            Barang = value[0]
+            P = int(value[1])
+            W = int(value[2])
+            set_objek[Barang] = {'w':W,'p':P}
+            if event == 'next':
+                i += 1
+                window.close()
+                continue
+        except:
+            err_msg = 'harap masukkan bilangan bulat'
+    window.close()
+
+    KP = Knapsack(kapasitas,set_objek=set_objek)
+    methods = ['Brute force','Greedy by profit','Greedy by weight', 'Greedy by density']
+    solusi = ''
+    TW = ''
+    TP = ''
+    metode = ''
+
+    while True:
+        layout = [
+                [sg.Text('Pilih metode')],
+                [sg.DropDown(methods),sg.Button('ok')], 
+                [sg.Text(f'hasil {metode}')],
+                [sg.Text('Himpunan solusi :'),sg.Text(solusi)],
+                [sg.Text('Total profit :'),sg.Text(TP)],
+                [sg.Text('Total weight :'),sg.Text(TW)],
+                [sg.Button('selesai')], 
+            ]
+        window = sg.Window(nama_aplikasi,layout)
+        event, value = window.read()
+        metode = value[0]
+        if event == sg.WIN_CLOSED or event == 'selesai':
+            break
+        if metode == 'Brute force':
+            S,TP,TW = KP.Bruteforce_Knapsack()
+            solusi = f'{S}'
+            TP = str(TP)
+            TW = str(TW)
+        elif metode == 'Greedy by profit':
+            S,TP,TW = KP.GreedyByProfit_Knapsack()
+            solusi = f'{S}'
+            TP = str(TP)
+            TW = str(TW)
+        elif metode == 'Greedy by weight':
+            S,TP,TW = KP.GreedyByWeight_Knapsack()
+            solusi = f'{S}'
+            TP = str(TP)
+            TW = str(TW)
+        elif metode == 'Greedy by density':
+            S,TP,TW = KP.GreedyByDensity_Knapsack()
+            solusi = f'{S}'
+            TP = str(TP)
+            TW = str(TW)
+    window.close()
     
